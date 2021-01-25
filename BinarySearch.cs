d728d883c2da7f71e9617961beb4362dab86a932
Gareth Whitley <me@garethwhitley.online>
2021-01-25 12:56:59 +0000

    updated comments and variables for clarity

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -7,23 +7,23 @@ namespace Sorting
     {
         public object Search(int[] arr, int target) // array must be sorted
         {
-            int lower = 0; // set the lower bounds
-            int higher = arr.Length - 1; // set the upper bounds
+            int lowerBounds = 0; 
+            int higherBounds = arr.Length - 1; 
             
-            while (lower <= higher) // stop after the two values collide -> we've looked at everything
+            while (lowerBounds <= higherBounds) // stop after the two values collide -> we've looked at everything
             {
-                int mid = (lower + higher) / 2; // find the mid point
-                if (target == arr[mid]) return ++mid; // if the element at the indexed mid point == the target, return the midpoint in 'real terms'
-                if (target < arr[mid])
+                int midpoint = (lowerBounds + higherBounds) / 2; // find the mid point
+                if (target == arr[midpoint]) return ++midpoint; // if the element at the indexed mid point == the target, return the midpoint in 'real terms'
+                if (target < arr[midpoint]) // if target is less than the current selected element, decrement the midpoint value
                 {
-                    higher = mid - 1; // if target is less than the current selected element, decrement the midpoint value
+                    higherBounds = midpoint - 1; 
                 }
-                else
+                else // if target is more than the current selected element, increment the midpoint value
                 {
-                    lower = mid + 1; // if target is more than the current selected element, increment the midpoint value
+                    lowerBounds = midpoint + 1; 
                 }
             }
-            return "None"; // if the target cannt be found, return the string "None"
+            return "None"; // target not found in the array?
         }
     }
 }
